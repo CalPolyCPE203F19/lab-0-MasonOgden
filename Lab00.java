f5b2eca82b4065421d131ee8ea169c0748629aa9
Mason Ogden <msogden@unix3.csc.calpoly.edu>
2019-09-20 16:59:20 -0700

    finished Lab00.java

--- Lab00.java
+++ Lab00.java
@@ -2,7 +2,39 @@ public class Lab00
 {
    public static void main(String[] args)
    {
+      // declaring and initializing some variables
+      int x = 5;
+      String y = "hello";
+      double z = 9.8;
 
+      // printing the variables
+      System.out.println("x: " + x + " y: " + y + " z: " + z);
+
+      // a list (make an array in java)
+      int[] nums = {3, 6, -1, 2};
+      for (int num : nums) {
+         System.out.println(num);
+      }
+
+      // call a function
+      int numFound = char_count(y, 'l');
+      System.out.println("Found: " + numFound);
+
+      // a counting for loop
+      for (int i = 1; i < 11; i++) {
+         System.out.print(i + " ");
+      }
+      
+      System.out.println(" ");
+      }
+      public static int char_count(String s, char c) {
+         int count = 0;
+         for (int j = 0; j < s.length(); j++) {
+            if (s.charAt(j) == c) {
+               count++;
+            }
+         }
+         return count;
+      }
    
-   }
 }
